5f05ea8f6caf210db2256be5f4050122a8991fb7
yeowonton <yeowonyoon0109@gmail.com>
2025-08-26 17:38:37 -0700

    Tailwind build + updated index/presidents/admin pages

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./public/**/*.html", "./public/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: { display: ['"Playfair Display"', 'serif'] },
+      colors: { brand:'#e91e63', brand600:'#d81b60', brand700:'#c2185b' },
+    },
+  },
+  plugins: [],
+};
